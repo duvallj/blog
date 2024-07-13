2612c7ca41b721951226c5b5982bd327fb29af6b
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-07-12 21:03:11 -0400

    fix eslint error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,3 @@
 /* eslint-disable */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-// Fill in types for capitalized versions of files
-declare module "*.JPG" {
-  export default import("astro").ImageMetadata;
-}
-declare module "*.PNG" {
-  export default import("astro").ImageMetadata;
-}
