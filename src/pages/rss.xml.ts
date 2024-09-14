09c7e4def4c33fde972bd08a8331a1ebfa22e019
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-09-14 18:38:15 -0400

    mdx in rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,25 +2,39 @@ import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { getPosts } from "../data/posts";
 import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { loadRenderers } from "astro:container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
 
 export async function GET(context: APIContext) {
+  // From https://blog.damato.design/posts/astro-rss-mdx/
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+
   const posts = await getPosts();
-  return rss({
-    title: "Jack Duvall's Blog",
-    description: "this is a site yes",
-    site: context.site ?? "https://blog.duvallj.pw/",
-    items: posts
-      .map((post) => ({
-        link: post.current.url,
-        content: sanitizeHtml(parser.render(post.entry.body)),
+  const items = await Promise.all(
+    posts.map(async (post) => {
+      const { Content } = await post.entry.render();
+      const content = await container.renderToString(Content);
+      const link = new URL(post.current.url, context.url.origin).toString();
+      return {
+        link,
+        content: sanitizeHtml(content, {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
         title: post.current.title,
         pubDate: post.current.date,
         description: post.frontmatter.description,
         categories: post.current.tags,
-      }))
-      .reverse(),
+      };
+    }),
+  );
+
+  return rss({
+    title: "Jack Duvall's Blog",
+    description: "this is a site yes",
+    site: context.site ?? "https://blog.duvallj.pw/",
+    items: items.reverse(),
     customData: `<language>en-us</language>`,
   });
 }
