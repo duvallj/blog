03353926ef639833c0ba429b7eaa9cf7379ea7e9
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-09-14 15:20:29 -0400

    better inline code snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -6,7 +7,7 @@ import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.duvallj.pw",
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
